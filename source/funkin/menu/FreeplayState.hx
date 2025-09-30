c3c12d49ff4b6746c76e3587c88807a4a4579fc1
infintie-kemonoyagi <cakefelino20@gmail.com>
2025-09-30 06:10:23 -0700

    refactor FreeplayState.hx adding FreeplayList and FreeplayGroup

--- source/funkin/menu/FreeplayState.hx
+++ source/funkin/menu/FreeplayState.hx
@@ -1,10 +1,24 @@
 package funkin.menu.freeplay;
 
+import funkin.backend.group.FunkinGroup.FunkinTypedGroup;
+import funkin.visuals.menu.MenuItem;
+import funkin.visuals.FunkinIcon;
+import funkin.visuals.text.FunkinText;
+import flixel.FlxSprite;
+import funkin.backend.group.FunkinSpriteGroup.FunkinTypedSpriteGroup;
+import funkin.backend.state.FunkinState;
 import flixel.util.FlxColor;
 
-class FreeplayState 
+class FreeplayState extends FunkinState
 {
-    
+    public var list:FreeplayList = null;
+
+    public override function create() 
+    {
+        super.create();
+
+        list = new FreeplayList(this);
+    }
 }
 
 typedef FreeplayData = 
@@ -16,4 +30,65 @@ typedef FreeplayData =
     var author:String;
     var difficulties:Array<String>;
     var rate:Int;
+}
+
+class FreeplayList
+{
+    private var list:Array<FreeplayData>;
+    public var group:FunkinTypedGroup<FreeplayGroup>;
+
+    public var length(get, never):Float;
+
+    public function new(parent:FreeplayState)
+    {
+        list = [];
+
+        group = new FunkinTypedGroup();
+        parent.add(group);
+    }
+
+    public function add(data:FreeplayData):FreeplayData 
+    {
+        list.push(data);
+        group.add(new FreeplayGroup(data, list.length - 1));
+
+        return data;
+    }
+
+    public inline function get(index:Int):FreeplayData
+    {
+        return list[index];
+    }
+
+    public inline function set(index:Int, data:FreeplayData):FreeplayData
+    {
+        return list[index] = data;
+    }
+
+    @:noCompletion
+    private inline function get_length():Float 
+    {
+        return list.length;
+    }
+}
+
+class FreeplayGroup extends FunkinTypedSpriteGroup<FlxSprite>
+{
+    public var text:MenuItem<FunkinText> = null;
+    public var icon:FunkinIcon = null;
+
+    public function new(data:FreeplayData, index:Int)
+    {
+        super();
+
+        text = new MenuItem(new FunkinText(data.name, "bold"));
+        text.animatePosition = true;
+        text.positionData = MenuItemPositionData.defaultAnim;
+        text.target = text.ID = index;
+        add(text.sprite);
+
+        icon = new FunkinIcon(data.icon);
+        icon.parent = text.sprite;
+        add(icon);
+    }
 }
