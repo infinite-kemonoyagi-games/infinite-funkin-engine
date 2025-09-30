b0dc1571bbba85908bc798adc8b03e97a1e6cbb4
infintie-kemonoyagi <cakefelino20@gmail.com>
2025-09-29 20:09:26 -0700

    first freeplay-simple commit creating the FreeplayState.hx file

---
+++ source/funkin/menu/FreeplayState.hx
@@ -0,0 +1,19 @@
+package funkin.menu.freeplay;
+
+import flixel.util.FlxColor;
+
+class FreeplayState 
+{
+    
+}
+
+typedef FreeplayData = 
+{
+    var name:String;
+    var icon:String;
+    var color:FlxColor;
+    var song:String;
+    var author:String;
+    var difficulties:Array<String>;
+    var rate:Int;
+}
